47bd85b6f2c340bca2f0e5a49328d952a9d3b958
Anna Nozhenko <nozhenko.ann@gmail.com>
2026-04-26 23:40:00 +0300

    Update 03_MAU.sql

--- sql/03_MAU.sql
+++ sql/03_MAU.sql
@@ -4,7 +4,8 @@
 -----------------------------------------------------------------------------------
 
 MAU рассчитан как количество уникальных аккаунтов, которые хотя бы один раз в месяц
-пользовались продуктом.
+пользовались продуктом. В данных обнаружены аномальные значения (использование продукта 
+до регистрации аккаунта). Такие значения не учитываются при расчете MAU.
 */
 
 
@@ -14,10 +15,15 @@ with active_months as (
         date_trunc('month', f.usage_date::date)::date as active_month
     from ravenstack_subscriptions s
     join ravenstack_feature_usage f using (subscription_id)
+    join ravenstack_accounts a using (account_id)
+    left join ravenstack_churn_events c using (account_id)
+    where f.usage_date::date >= a.signup_date::date and
+    	(c.churn_date is null or 
+    	f.usage_date::date <= churn_date::date)
 )
 select 
-    active_month,
-    count(distinct account_id) as mau
+	active_month,
+	count(distinct account_id) as mau
 from active_months
 group by active_month
 order by 1;
